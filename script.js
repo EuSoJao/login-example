2b6859251cf32f230255883cf9aa3aa62d2072f6
EuSoJao <89408008+EuSoJao@users.noreply.github.com>
2024-08-27 11:54:48 -0300

    ted

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const aviso1 = document.querySelector("button")
-
-aviso1.addEventListener("click", function aviso1() {
-    alert("HMMMMMM, *PLIN*")
-})
-
-function aviso1() {
+function aviso() {
     alert("HMMMMMM, *PLIN*")
 }
