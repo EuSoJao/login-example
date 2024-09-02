3e5b7a0a5bd174d653b31cb1768213e987bca8b4
EuSoJao <89408008+EuSoJao@users.noreply.github.com>
2024-09-02 08:37:59 -0300

    test

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function loadMessage(){
     window.onload = ("load", () => {
         window.alert("Log-In or Create a New Account")
     })
-}
 
 function alerta() {
     var input = document.getElementById("myInput");
