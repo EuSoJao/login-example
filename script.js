d9d979f91ad5ba45385cf62cd4714cf0bbfa7d4e
EuSoJao <89408008+EuSoJao@users.noreply.github.com>
2024-09-02 08:34:23 -0300

    teste

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function loadMessage(){
+    window.onload = ("load", (console.log('teste')))
+}
+
 function alerta() {
     var input = document.getElementById("myInput");
     var inputValue = input.value;
